ee6b51f01be680abe79d68063b6e2faea92a6f9d
nirnay01 <ravalnirnay@gmail.com>
2022-08-09 22:43:13 +0530

    practical 1 completed

--- src/main/kotlin/practical1.kt
+++ src/main/kotlin/practical1.kt
@@ -1,8 +1,10 @@
 fun main()
 {
     val x=300
-    var y:Int =200
-    val z:Int =100
-    println("x=$x \ny=$y \nz=$z")
+    var y:Double=5.5
+    val z:Float=x.toFloat()
+    val a:Char='a'
+    val s:String="abc"
+    println("x=$x\ny=$y\nz=$z\na=$a\ns=$s")
     
 }
