d75efb636692b6efe096b440fac80e3fc210416b
nirnay01 <ravalnirnay@gmail.com>
2022-08-09 22:33:54 +0530

    practical 2 completed

--- src/main/kotlin/practical2.kt
+++ src/main/kotlin/practical2.kt
@@ -1,5 +1,11 @@
 fun main()
 {
     val x:Int =100
-    println("x in interger:$x")
+    val y:Float=x.toFloat()
+    val z:Double=x.toDouble()
+    val a:Char=x.toChar()
+    val b:Byte=x.toByte()
+    val c:Long=x.toLong()
+    val d:String=x.toString()
+    println("Int:$x\nFloat:$y\nDouble:$z\nCharater:$a\nByte:$b\nLong:$c\nString:$d")
 }
